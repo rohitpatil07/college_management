a4e637d95d98e30308d2c7f114e318e656452ecc
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2022-12-01 11:08:42 +0530

    Completed Student Navigations

--- components/NavBar/StudentNavigations/MobileNav.tsx
+++ components/NavBar/StudentNavigations/MobileNav.tsx
@@ -1,9 +1,59 @@
+'use client';
 import React from 'react'
-
+import { useState } from 'react';
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>MobileNav</div>
+    <div className='flex sm:hidden flex-col w-full bg-white text-black drop-shadow-lg'>
+      <div className="flex flex-row w-full justify-between items-center p-4 border-b-2">
+        DYPLOGO
+        <div className="flex flex-row">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>}
+          </button>
+          &nbsp;&nbsp;
+          <button className="border-solid rounded-full border-2 border-black">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      {isOpen ?
+        <>
+          <div className="flex flex-row w-full justify-between items-center p-4 border-b-2">
+            <button className="flex flex-row items-center text-base text-slate-700 font-medium">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+                <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+              </svg>&nbsp;&nbsp;&nbsp;
+              Messages
+            </button>
+          </div>
+          <div className="flex flex-row w-full justify-between items-center p-4 border-b-2">
+            <button className="flex flex-row items-center text-base text-slate-700 font-medium">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                <path fillRule="evenodd" d="M10 2a6 6 0 00-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 00.515 1.076 32.91 32.91 0 003.256.508 3.5 3.5 0 006.972 0 32.903 32.903 0 003.256-.508.75.75 0 00.515-1.076A11.448 11.448 0 0116 8a6 6 0 00-6-6zM8.05 14.943a33.54 33.54 0 003.9 0 2 2 0 01-3.9 0z" clipRule="evenodd" />
+              </svg>&nbsp;&nbsp;&nbsp;
+              Notifications
+            </button>
+          </div>
+          <div className="flex flex-row w-full justify-between items-center p-4 border-b-2">
+            <button className="flex flex-row items-center text-base text-slate-700 font-medium">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                <path fillRule="evenodd" d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z" clipRule="evenodd" />
+              </svg>&nbsp;&nbsp;&nbsp;
+              Calendar
+            </button>
+          </div>
+        </>
+        : <></>}
+    </div>
   )
 }
 
-export default MobileNav
+export default MobileNav;
