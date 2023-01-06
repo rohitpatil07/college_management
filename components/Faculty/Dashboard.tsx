5012a300121411b6907bb143084c44f1c0ff9219
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-01-06 22:55:54 +0530

    Designed faculty side dashboard and subject page

--- components/Faculty/Dashboard.tsx
+++ components/Faculty/Dashboard.tsx
@@ -5,12 +5,13 @@ import { useAuth } from "../../contexts/AuthContext";
 import Loading from "../Loaders/Loading";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 const Dashboard = () => {
-    const router = useRouter();
-    const AuthData: any = useAuth();
-    const[subjects,setSubjects]:any=useState(null);
-    const get_subject=async()=>{
-        const response = await axios.get(
+	const router = useRouter();
+	const AuthData: any = useAuth();
+	const [subjects, setSubjects]: any = useState(null);
+	const get_subject = async () => {
+		const response = await axios.get(
 			'http://localhost:5000/lms/filter/allSubjects',
 			{
 				headers: {
@@ -19,37 +20,67 @@ const Dashboard = () => {
 				},
 			}
 		);
-    console.log(response);
-    setSubjects(response.data);
-    }
-    useEffect(() => {
-        get_subject();
-      }, []);
+		console.log(response);
+		setSubjects(response.data);
+	}
+	useEffect(() => {
+		get_subject();
+	}, []);
 	return (
 		<div className="w-full flex justify-center items-center align-middle">
-			<div className="flex bg-white w-10/12 mt-5 flex-col pt-8 items-center rounded-2xl drop-shadow-lg">
-				<h3 className="text-xl sm:text-2xl font-bold text-gray-900">
+			<div className="flex bg-slate-100 sm:bg-white w-full sm:w-11/12 mt-5 flex-col pt-8 items-center sm:rounded-2xl sm:drop-shadow-lg">
+			<div className="w-11/12 mx-auto flex flex-col  justify-around container py-3 text-slate-500 font-medium">
+        <Link href='/faculty/dashboard' className="flex flex-row items-center pb-2 mb-1 border-b border-slate-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-4 h-4 mr-2"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Home / Dashboard
+        </Link>
+      </div>
+				<h3 className="text-xl sm:text-2xl font-medium text-gray-900">
 					All Subjects
 				</h3>
-                {subjects ? <>
-                    {subjects.map(({ subject_id, subject_name, semester, department, batch, type }: any, x: number) =>
-                    <div key={subject_id}>
-                        <h3 className="text-lg sm:text-xl font-bold text-gray-900">{subject_name}</h3>
-                        <Link href={{
-							pathname:"/faculty/subject",
-							query:
-							{
-								subject_id:subject_id
-							}
-						}} className="p-1 mb-3 md:mb-0 text-sm w-48 bg-white text-slate-900 font-semibold border-2 border-slate-900 rounded-md">
-							Check Here For More Info
-						</Link>
-                    </div>
-                    )}
-                </> :
-                 <>
-                 <Loading loadState='loading'/>
-                 </>}
+				<div className='border-t-4 my-2 py-3 w-11/12 flex flex-row flex-wrap items-center justify-between'>
+					<button className="flex items-center p-2 w-fit px-4 py-2 rounded-lg bg-accent text-white hover:scale-105 transition-all">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-1">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+						</svg>
+						Add New Subject
+					</button>
+					<button className="mt-1 sm:mt-0 p-2 w-fit px-4 py-2 rounded-md bg-accent text-white hover:scale-105 transition-all">
+						Filter Subjects
+					</button>
+				</div>
+				{subjects ? <div className='flex flex-col md:flex-row justify-evenly items-center w-full mb-5'>
+					{subjects.map(({ subject_id, subject_name, semester, department, batch, type }: any, i: number) =>
+						<div key={subject_id} className='flex flex-col items-center w-10/12 scale-90 md:scale-100 md:w-72 xl:w-2/5 xl:scale-90 shadow-2xl drop-shadow-2xl rounded-xl overflow-hidden bg-white'>
+							<img src={`/subjects/subject${i + 1}.jpg`} alt={subject_name} className="w-full min-h-[10rem] object-cover rounded-xl" />
+							<div className="text-lg sm:text-xl font-medium text-gray-900 my-4 text-center">{subject_name}</div>
+							<Link href={{
+								pathname: "/faculty/subject",
+								query:
+								{
+									subject_id: subject_id,
+									subject_name: subject_name
+								}
+							}} className="mb-4 w-fit mx-auto px-16 py-2 rounded-full bg-accent text-white hover:scale-105 transition-all">
+								Open
+							</Link>
+						</div>
+					)}
+				</div> :
+					<>
+						<Loading loadState='loading' />
+					</>}
 			</div>
 		</div>
 	);
