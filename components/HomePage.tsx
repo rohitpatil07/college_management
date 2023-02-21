b6605068dfc51c9c195abb5092aa7a0e9e9078e6
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-02-21 18:53:03 +0530

    New welcome modal added

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -3,99 +3,97 @@ import React from "react";
 import Link from "next/link";
 import { useAuth } from "../contexts/AuthContext";
 const HomePage = () => {
-	return (
-		<div className="xl:w-2/3 sm:shadow-2xl sm:rounded-xl sm:p-3 flex flex-col justify-around items-center sm:container sm:my-5 sm:mx-auto border-solid w=48rem sm:bg-white">
-			<div className="w-10/12 sm:w-11/12 border-b border-slate-300 pt-5">
-				<button className="flex flex-row text-base text-slate-500 font-medium items-center">
-					<svg
-						className="mr-2 w-4 h-4"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-					>
-						<path
-							fillRule="evenodd"
-							d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
-							clipRule="evenodd"
-						/>
-					</svg>
-					Home
-				</button>
-			</div>
-			<br />
-			<div className="flex flex-col sm:flex-row justify-evenly items-center w-full mb-5">
-				<div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
-					<img
-						className="w-full min-h-[15rem] object-cover bg-zinc-300"
-						src={`/tpc.png`}
-						alt="tpc"
-					/>
-					<div className="py-9 px-5 flex flex-col gap-3 items=center text-center">
-						<span className="text-xl font-bold text-gray-900">
-							Marketplace
-						</span>
-						{/* <span className="text-slate-500 text-sm">
+  return (
+    <div className="xl:w-2/3 sm:shadow-2xl sm:rounded-xl sm:p-3 flex flex-col justify-around items-center sm:container sm:my-5 sm:mx-auto border-solid w=48rem sm:bg-white">
+      <div className="w-10/12 sm:w-11/12 border-b border-slate-300 pt-5">
+        <button className="flex flex-row text-base text-slate-500 font-medium items-center">
+          <svg
+            className="mr-2 w-4 h-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Home
+        </button>
+      </div>
+      <br />
+      <div className="flex flex-col sm:flex-row justify-evenly items-center w-full mb-5">
+        <div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
+          <img
+            className="w-full min-h-[15rem] object-cover bg-zinc-300"
+            src={`/tpc.png`}
+            alt="tpc"
+          />
+          <div className="py-9 px-5 flex flex-col gap-3 items=center text-center">
+            <span className="text-xl font-bold text-gray-900">Marketplace</span>
+            {/* <span className="text-slate-500 text-sm">
 							Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
 						</span> */}
-						<Link
-							href="/marketplace"
-							className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white transition-all hover:scale-105"
-						>
-							Go To Marketplace
-						</Link>
-					</div>
-				</div>
-				<br />
-				<div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
-					<img
-						className="w-full min-h-[10rem] object-cover bg-zinc-300"
-						src={`/lms.png`}
-						alt="lms"
-					/>
-					<div className="p-5 flex flex-col gap-3 items=center text-center">
-						<span className="text-xl font-bold text-gray-900">
-							Learning Management
-							<br />
-							System
-						</span>
-						{/* <span className="text-slate-500 text-sm">
+            <Link
+              href="/marketplace"
+              className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white transition-all hover:scale-105"
+            >
+              Go To Marketplace
+            </Link>
+          </div>
+        </div>
+        <br />
+        <div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
+          <img
+            className="w-full min-h-[10rem] object-cover bg-zinc-300"
+            src={`/lms.png`}
+            alt="lms"
+          />
+          <div className="p-5 flex flex-col gap-3 items=center text-center">
+            <span className="text-xl font-bold text-gray-900">
+              Learning Management
+              <br />
+              System
+            </span>
+            {/* <span className="text-slate-500 text-sm">
 							Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
 						</span> */}
-						<Link
-							href="/lms/dash"
-							className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white hover:scale-105 transition-all"
-						>
-							Go To LMS
-						</Link>
-					</div>
-				</div>
-				<br />
-				<div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
-					<img
-						className="w-full min-h-[10rem] object-cover bg-zinc-300"
-						src={`/tpc.png`}
-						alt="tpc"
-					/>
-					<div className="p-5 flex flex-col gap-3 items=center text-center">
-						<span className="text-xl font-bold text-gray-900">
-							Training And Placement
-							<br />
-							Cell
-						</span>
-						{/* <span className="text-slate-500 text-sm">
+            <Link
+              href="/lms/dash"
+              className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white hover:scale-105 transition-all"
+            >
+              Go To LMS
+            </Link>
+          </div>
+        </div>
+        <br />
+        <div className="w-10/12 scale-90 sm:scale-100 sm:w-72 xl:w-1/3 xl:scale-90 shadow-xl rounded-xl overflow-hidden bg-white border-slate-300 border-solid border-2">
+          <img
+            className="w-full min-h-[10rem] object-cover bg-zinc-300"
+            src={`/tpc.png`}
+            alt="tpc"
+          />
+          <div className="p-5 flex flex-col gap-3 items=center text-center">
+            <span className="text-xl font-bold text-gray-900">
+              Training And Placement
+              <br />
+              Cell
+            </span>
+            {/* <span className="text-slate-500 text-sm">
 							Lorem ipsum dolor sit amet pretium consectetur adipiscing elit.
 						</span> */}
-						<Link
-							href="/tpc/editProfile/personalInfo"
-							className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white transition-all hover:scale-105"
-						>
-							Go To TPC
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <Link
+              href="/tpc"
+              className="p-2 w-fit mx-auto px-5 rounded-3xl bg-accent text-white transition-all hover:scale-105"
+            >
+              Go To TPC
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -8,151 +8,145 @@ import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 
 function LoginPage() {
-	const router = useRouter();
-	const server=process.env.NEXT_PUBLIC_SERVER_URL;
-	const [email, setEmail] = useState("");
-	const [role, setRole] = useState("student");
-	const [password, setPassword] = useState("");
+  const router = useRouter();
+  const server = process.env.NEXT_PUBLIC_SERVER_URL;
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("student");
+  const [password, setPassword] = useState("");
 
-	const AuthData: any = useAuth();
+  const AuthData: any = useAuth();
 
-	const fpassword = async () => {
-		if (email)
-		{
-			const response = await axios({
-				method: 'post',
-				url: `${server}/auth/forgot_mail`,
-				headers: {
-					'Content-Type': 'application/json',
-				}, 
-				data: {
-					email: email,
-					role:role
-				}
-				});
-			Swal.fire({
-				icon: "info",
-				title: `E-mail sent to ${email} to recover password`,
-				showConfirmButton: false,
-				timer: 1500,
-			});
+  const fpassword = async () => {
+    if (email) {
+      const response = await axios({
+        method: "post",
+        url: `${server}/auth/forgot_mail`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email: email,
+          role: role,
+        },
+      });
+      Swal.fire({
+        icon: "info",
+        title: `E-mail sent to ${email} to recover password`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: `Please enter your email address`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
 
-		}
-		else
-		{
-			Swal.fire({
-				icon: "error",
-				title: `Please enter your email address`,
-				showConfirmButton: false,
-				timer: 1500,
-			});
-		}
+  const handleSubmit = async () => {
+    const data = { email, role, password };
+    const response = await AuthData.login(data);
+    if (
+      response.data == "You are not authorized" ||
+      Object.keys(response.data).length == 0 ||
+      response.data.length == 0
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Login Failed,Incorrect Credentials",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      const { token } = response.data;
+      const userRole: string = response.data.user.role;
 
-	}
+      if (token) {
+        console.log(response);
+        Swal.fire({
+          icon: "success",
+          title: "Login Successfull",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        if (userRole == "student") router.push("/home");
+        else if (userRole == "faculty") router.push("/faculty/dashboard");
+        else if (userRole == "admin") router.push("/admin");
+        else if (userRole == "lms_admin") router.push("/lms_admin");
+        else router.push("/company");
+      }
+    }
+  };
 
-
-	const handleSubmit = async () => {
-		const data = { email, role, password };
-		const response = await AuthData.login(data);
-		if (response.data=="You are not authorized" || Object.keys(response.data).length==0 || response.data.length==0)
-		{
-			Swal.fire({
-				icon: "error",
-				title: "Login Failed,Incorrect Credentials",
-				showConfirmButton: false,
-				timer: 1500,
-			});
-	}
-		else
-		{
-			
-				const { token } = response.data;
-				const userRole: string = response.data.user.role;
-			
-				if (token) {
-					console.log(response);
-					Swal.fire({
-						icon: "success",
-						title: "Login Successfull",
-						showConfirmButton: false,
-						timer: 1500,
-					});
-					if (userRole == "student") router.push("/home");
-					else if (userRole == "faculty") router.push("/faculty/dashboard");
-					else if (userRole == "admin") router.push("/admin/lookup");
-					else if (userRole == "lms_admin") router.push("/lms_admin/lookup");
-					else router.push("/company");
-				}
-		}
-	};
-
-	return (
-		<>
-			<div>
-				<div className="flex w-full justify-center md:justify-start">
-					<img
-						src={`/logo.png`}
-						alt={`logo`}
-						className="w-1/2 my-3 mx-1 scale-90 md:w-1/4 xl:w-1/6"
-					/>
-				</div>
-				<div className="flex flex-col bg-white w-10/12 xl:w-1/2 drop-shadow-lg m-auto items-center py-10 rounded-2xl mb-2">
-					<h1 className="font-bold text-xl">Sign in to account</h1>
-					<form
-						className="flex flex-col items-start w-3/4 mt-4 gap-2"
-						action=""
-					>
-						<label htmlFor="userIdentity">Sign in as:</label>
-						<select
-							name="userIdentity"
-							className="w-full border-2 px-5 py-4 rounded-xl"
-							id=""
-							onChange={(e) => setRole(e.target.value)}
-						>
-							<option value="student">Student</option>
-							<option value="faculty">Faculty</option>
-							<option value="admin">Admin</option>
-							<option value="lms_admin">LMS Admin</option>
-							<option value="company">Company</option>
-						</select>
-						<label className="mt-6" htmlFor="email">
-							Email Address
-						</label>
-						<input
-							type="text"
-							className="w-full border px-5 py-3 rounded-xl outline-none"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-						<label htmlFor="password">Your Password</label>
-						<input
-							type="password"
-							className="w-full border px-5 py-3 rounded-xl outline-none"
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-						<button
-							type="button"
-							className="btn btn-outline-primary w-full bg-accent text-white rounded-xl px-5 py-3 mt-8 hover:scale-105 transition-all"
-							onClick={handleSubmit}
-						>
-							Submit
-						</button>
-						<div className="flex flex-col md:flex-row w-full md:justify-between">
-							<div className="flex my-3 md:my-0 gap-1 justify-center">
-								<input type="checkbox" name="remember" id="" />
-								<label htmlFor="remember">Remember Me</label>
-							</div>
-							<button
-								className="flex justify-center hover:text-blue-500"
-								onClick={fpassword}
-							>
-								Forgot Password?
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <div className="flex w-full justify-center md:justify-start">
+          <img
+            src={`/logo.png`}
+            alt={`logo`}
+            className="w-1/2 my-3 mx-1 scale-90 md:w-1/4 xl:w-1/6"
+          />
+        </div>
+        <div className="flex flex-col bg-white w-10/12 xl:w-1/2 drop-shadow-lg m-auto items-center py-10 rounded-2xl mb-2">
+          <h1 className="font-bold text-xl">Sign in to account</h1>
+          <form
+            className="flex flex-col items-start w-3/4 mt-4 gap-2"
+            action=""
+          >
+            <label htmlFor="userIdentity">Sign in as:</label>
+            <select
+              name="userIdentity"
+              className="w-full border-2 px-5 py-4 rounded-xl"
+              id=""
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value="student">Student</option>
+              <option value="faculty">Faculty</option>
+              <option value="admin">Admin</option>
+              <option value="lms_admin">LMS Admin</option>
+              <option value="company">Company</option>
+            </select>
+            <label className="mt-6" htmlFor="email">
+              Email Address
+            </label>
+            <input
+              type="text"
+              className="w-full border px-5 py-3 rounded-xl outline-none"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              className="w-full border px-5 py-3 rounded-xl outline-none"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-primary w-full bg-accent text-white rounded-xl px-5 py-3 mt-8 hover:scale-105 transition-all"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+            <div className="flex flex-col md:flex-row w-full md:justify-between">
+              <div className="flex my-3 md:my-0 gap-1 justify-center">
+                <input type="checkbox" name="remember" id="" />
+                <label htmlFor="remember">Remember Me</label>
+              </div>
+              <button
+                className="flex justify-center hover:text-blue-500"
+                onClick={fpassword}
+              >
+                Forgot Password?
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
